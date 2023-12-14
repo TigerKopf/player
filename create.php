6768b5d9a1e88b7bc7363d246eed01d22c77395f
TigerKopf <79372170+TigerKopf@users.noreply.github.com>
2023-12-14 12:08:20 +0100

    add basic

---
+++ create.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Characteristics create</title>
+</head>
+<body>
+
+<h1>on this page you can create a Characteristics for a friend</h1>
+<br>
+<form action="send_create.php" method="post">
+        <label for="author">Author:</label>
+        <input type="text" name="author" required><br>
+
+        <label for="name">Name:</label>
+        <input type="text" name="name" required><br>
+
+        <label for="nickname">Spitzname:</label>
+        <input type="text" name="nickname"><br>
+
+        <label for="birthday">Geburtstag:</label>
+        <input type="date" name="birthday"><br>
+
+        <label for="street">Straße:</label>
+        <input type="text" name="street"><br>
+
+        <label for="postal_code">PLZ:</label>
+        <input type="text" name="postal_code"><br>
+
+        <label for="city">Ort:</label>
+        <input type="text" name="city"><br>
+
+        <label for="tel">Telefon:</label>
+        <input type="tel" name="tel"><br>
+
+        <label for="email">E-Mail:</label>
+        <input type="email" name="email"><br>
+
+        <label for="kindergartengruppe">Kindergartengruppe:</label>
+        <input type="text" name="kindergartengruppe"><br>
+
+        <label for="favorite_color">Lieblingsfarbe:</label>
+        <input type="text" name="favorite_color"><br>
+
+        <label for="favorite_animal">Lieblingstier:</label>
+        <input type="text" name="favorite_animal"><br>
+
+        <label for="favorite_food">Lieblingsessen:</label>
+        <input type="text" name="favorite_food"><br>
+
+        <label for="favorite_dessert">Lieblingsnachtisch:</label>
+        <input type="text" name="favorite_dessert"><br>
+
+        <label for="favorite_music">Lieblingsmusik:</label>
+        <input type="text" name="favorite_music"><br>
+
+        <label for="favorite_song">Lieblingslied:</label>
+        <input type="text" name="favorite_song"><br>
+
+        <label for="favorite_game">Lieblingsspiel:</label>
+        <input type="text" name="favorite_game"><br>
+
+        <label for="love">Love:</label>
+        <input type="text" name="love"><br>
+
+        <label for="not_love">Not Love:</label>
+        <input type="text" name="not_love"><br>
+
+        <label for="wish">Wish:</label>
+        <input type="text" name="wish"><br>
+
+        <input type="submit" value="Daten speichern">
+    </form>
+</body>
+</html>
